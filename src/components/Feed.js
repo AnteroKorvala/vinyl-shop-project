3dd925dcf922c55c2632dd5f87ee4ca8cc76f458
D22126473 Aleksi Hannula <D22126473@mydit.ie>
2022-11-18 21:21:53 +0000

    Initialized feed and local data

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Feed() {
+  const [data, setData] = useState([])
+
+  const getData = () => {
+    fetch('./data.json',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      .then(function(response){
+        console.log(response)
+        return response.json()
+      })
+      .then(function(myJson){
+        console.log(myJson)
+        setData(myJson)
+      })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div>Feed</div>
+    <div>{data}</div>
   )
 }
 
