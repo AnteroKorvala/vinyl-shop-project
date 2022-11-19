cc354375985af23a95ad9facfe896cd2e15239d8
D22126473 Aleksi Hannula <D22126473@mydit.ie>
2022-11-19 16:26:39 +0000

    Feed cards

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import './Feed.css'
+import belowtheheavens from './images/belowtheheavens.png'
+import numberofthebeast from './images/numberofthebeast.png'
+import antichristsuperstar from './images/antichristsuperstar.png'
+import czarmageddon from './images/czarmageddon.png'
 
 function Feed() {
   const [data, setData] = useState([])
@@ -16,8 +21,8 @@ function Feed() {
         return response.json()
       })
       .then(function(myJson){
-        console.log(myJson)
-        setData(myJson)
+        console.log(myJson.records)
+        setData(myJson.records)
       })
   }
 
@@ -26,7 +31,24 @@ function Feed() {
   }, [])
 
   return (
-    <div>{data}</div>
+    <>
+    <div className='feed'>
+      {data && data.length > 0 &&
+      data.map((item) => {
+        return (
+          <div key={item.id} className='item-container'>
+            <img className='item-image' src={item.image.split("")} alt='bth' />
+            <div className='item-name'>{item.name}</div>
+            <div className='item-artist'>{item.artist}</div>
+            <div className='item-genre'>{item.genre}</div>
+            <div className='item-released'>{item.released}</div>
+            <div className='item-length'>{item.length}</div>
+            <div className='item-producer'>{item.producer}</div>
+          </div>
+        )
+      })}
+    </div>
+    </>
   )
 }
 
--- src/components/Vinyl.js
+++ src/components/Vinyl.js
@@ -9,4 +9,11 @@ function Vinyl() {
   )
 }
 
+/*
+This vinyl record component was inspired by the artwork of one Ana Tudor: https://codepen.io/thebabydino/pen/kNqgKO (@thebabydino on Codepen -> https://codepen.io/thebabydino).
+They had made this cool vinyl record purely using CSS and we thought it fit perfectly with the theme of our project.
+We added some extra flair to it by modifying the code a little and giving it a spinning animation and making it a little wobbly, similiar to a real vinyl record.
+We got the inspiration for this animation from the amazing CSS loaders that one Temani Afif makes -> https://dev.to/afif/i-made-100-css-loaders-for-your-next-project-4eje.
+*/
+
 export default Vinyl
