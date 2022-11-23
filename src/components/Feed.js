f6aef07d19efc254aafebba5f5740e37251890a9
D22126473 Aleksi Hannula <D22126473@mydit.ie>
2022-11-23 12:48:29 +0000

    Added popup

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom"
 import './Feed.css'
 import images from './Images.js'
 import Vinyl from './Vinyl'
+import Popup from './Popup'
+import CloseIcon from '@mui/icons-material/Close'
 
 /*
 TODO
@@ -13,6 +15,8 @@ Create carousel component
 function Feed() {
   const navigate = useNavigate()
   const [data, setData] = useState([])
+  const [isOpen, setIsOpen] = useState(true)
+  const togglePopup = () => setIsOpen(!isOpen)
 
   const getData = () => {
     fetch('./data.json',
@@ -23,11 +27,9 @@ function Feed() {
         }
       })
       .then(function(response){
-        console.log(response)
         return response.json()
       })
       .then(function(myJson){
-        console.log(myJson.records)
         setData(myJson.records)
       })
   }
@@ -37,34 +39,45 @@ function Feed() {
   }, [])
 
   function ChooseImage(id) {
-    console.log(images)
-    console.log(id)
     return images[id]
   }
 
   return (
     <div className='feed-container'>
       <div className='feed'>
-        {data && data.length > 0 &&
-        data.map((item) => {
-          return (
-            <div
-              key={item.id}
-              className='item-container'
-              onClick={() => {
-                navigate(`/recordID/${item.id}`)
-              }}
-            >
-              <img className='item-image' src={ChooseImage(item.id)} alt='album-cover' />
-              <div className='item-name'>{item.name}</div>
-              <div className='item-artist'>by  {item.artist}</div>
-              <div className='item-genre'>Genre: {item.genre}</div>
-              <div className='item-released'>Released: {item.released}</div>
+      {isOpen && <div className='popup'>
+        <Popup
+          content = {<>
+            <div className='popup-text'>
+              Scroll down on albums<br />
+              to see extra details!<br />
             </div>
-          )
-        })}
+            {<CloseIcon className='popup-close' onClick = {togglePopup} />}
+          </>}
+        />
+      </div>}
+        <div className='all-albums-container'>
+          {data && data.length > 0 &&
+          data.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className='item-container'
+                onClick={() => {
+                  navigate(`/recordID/${item.id}`)
+                }}
+              >
+                <img className='item-image' src={ChooseImage(item.id)} alt='album-cover' />
+                <div className='item-name'>{item.name}</div>
+                <div className='item-artist'>by  {item.artist}</div>
+                <div className='item-genre'>Genre: {item.genre}</div>
+                <div className='item-released'>Released: {item.released}</div>
+              </div>
+            )
+          })}
+        </div>
+        <Vinyl />
       </div>
-      <Vinyl />
     </div>
   )
 }
