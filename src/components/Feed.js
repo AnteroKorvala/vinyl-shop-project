7f1de347ad76a066c329c00790bb5d2b22ac1e91
D22126473 Aleksi Hannula <D22126473@mydit.ie>
2022-11-23 14:10:15 +0000

    Commented out carousel component due to bug on refresh

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,6 +5,7 @@ import images from './Images.js'
 import Vinyl from './Vinyl'
 import Popup from './Popup'
 import CloseIcon from '@mui/icons-material/Close'
+import Carousel, {CarouselItem} from './Carousel'
 
 /*
 TODO
@@ -45,17 +46,30 @@ function Feed() {
   return (
     <div className='feed-container'>
       <div className='feed'>
-      {isOpen && <div className='popup'>
-        <Popup
-          content = {<>
-            <div className='popup-text'>
-              Scroll down on albums<br />
-              to see extra details!<br />
-            </div>
-            {<CloseIcon className='popup-close' onClick = {togglePopup} />}
-          </>}
-        />
-      </div>}
+        {isOpen && <div className='popup'>
+          <Popup
+            content = {<>
+              <div className='popup-text'>
+                Scroll down on albums<br />
+                to see extra details!<br />
+              </div>
+              {<CloseIcon className='popup-close' onClick = {togglePopup} />}
+            </>}
+          />
+        </div>}
+        {/* <div className='carousel'>
+          <Carousel>
+            {data && data.length > 0 &&
+            data.map((item) => {
+              return (
+                <CarouselItem>
+                  <img className='item-image-carousel' src={ChooseImage(item.id)} alt='album-cover'/>
+                </CarouselItem>
+              )
+            })
+            }
+          </Carousel>
+        </div> */}
         <div className='all-albums-container'>
           {data && data.length > 0 &&
           data.map((item) => {
