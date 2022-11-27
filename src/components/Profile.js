560128613b4daa32a3ffd9c97b2bd03ff070d811
D22126473 Aleksi Hannula <D22126473@mydit.ie>
2022-11-27 12:04:05 +0000

    Created profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,81 @@
-import { FormControl, FormHelperText, InputLabel, Input } from '@mui/material'
-import React from 'react'
+import { FormControl, FormHelperText, InputLabel, Input, Button } from '@mui/material'
+import React, {useState} from 'react'
 import './Profile.css'
 import AccountCircleIcon from "@mui/icons-material/AccountCircle"
 
 function Profile() {
+  const [username, setUsername] = useState("Haxeli")
+  const [email, setEmail] = useState("haxeli@gmail.com")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [image, setImage] = useState()
+
+  function UpdateProfile({username, email, password}) {
+    console.log('update')
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    UpdateProfile({username, email, password})
+  }
+
   return (
     <div className='profile-container'>
       <div className='form-profile'>
         <div className='form-profile-icon'>
-          <AccountCircleIcon sx={{fontSize: "5em"}} />
+          {!image && <AccountCircleIcon sx={{fontSize: "5em"}} />}
         </div>
         <FormControl className='form-item'>
-          <InputLabel htmlFor='username'>Username</InputLabel>
-          <Input id="username" aria-describedby="username-helper" />
+          <InputLabel htmlFor='username'></InputLabel>
+          <Input
+            id="username"
+            type='username'
+            aria-describedby="username-helper"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <FormHelperText id="username-helper">Don't choose a dumb name</FormHelperText>
         </FormControl>
         <FormControl className='form-item'>
-          <InputLabel htmlFor='password'>Password</InputLabel>
-          <Input id="password" aria-describedby="password-helper" />
+          <InputLabel htmlFor='email'></InputLabel>
+          <Input
+            id="email"
+            type='email'
+            aria-describedby="email-helper"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormHelperText id="email-helper">Did you create that email when you were 12?</FormHelperText>
+        </FormControl>
+        <FormControl className='form-item'>
+          <InputLabel htmlFor='password'></InputLabel>
+          <Input
+            id="password"
+            type='password'
+            placeholder="Password"
+            aria-describedby="password-helper"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <FormHelperText id="password-helper">We'll always share your password</FormHelperText>
         </FormControl>
+        <FormControl className='form-item'>
+          <InputLabel htmlFor='password'></InputLabel>
+          <Input
+            id="password"
+            type='password'
+            placeholder="Confirm password"
+            aria-describedby="password-helper"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <FormHelperText id="password-helper">We'll always share your password</FormHelperText>
+        </FormControl>
+        <Button
+          variant='primary'
+          type='submit'
+          onClick={submitHandler}
+        >
+          Update
+        </Button>
       </div>
     </div>
   )
