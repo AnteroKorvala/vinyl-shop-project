b1c1a8025ce0fc688e06977b589a4cea7df0249e
D22126473 Aleksi Hannula <D22126473@mydit.ie>
2022-11-27 22:16:40 +0000

    latest

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@ import vinylRecords from './routes/vinylRecords.js';
 import userRecords from "./routes/userRecords.js";
 import wishlistRecords from './routes/wishlistRecords.js';
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With, Content-type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 
 app.use('/', vinylRecords);
 app.use('/u/', userRecords);
